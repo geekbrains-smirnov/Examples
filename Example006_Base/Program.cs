f35a2de42fcdbcefec11e7ce04be861910b599c7
Aryom Smirnov <expertvideo.as@gmail.com>
2022-06-22 17:16:44 +0300

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int c = 7;
+int d = 10;
+int e = 3;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Максимальное значение: ");
+Console.WriteLine(max);
